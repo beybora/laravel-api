1594a8e5feb6578f53994fb3f1040cee97749789
beybora <bora.b.sarikaya@gmail.com>
2025-05-13 19:18:44 +0200

    Add authentication routes for register, login, and logout

--- api_01/routes/api.php
+++ api_01/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::resource('users', UserController::class);
 
 Route::resource('posts', PostController::class);
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
